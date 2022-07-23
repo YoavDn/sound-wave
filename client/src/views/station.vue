08161783ec4f34ef80156254eb3903899821a0c5
yoav <yoavdan.m@gmail.com>
2022-07-23 23:10:11 +0300

    added popupmag

--- client/src/views/station.vue
+++ client/src/views/station.vue
@@ -71,6 +71,13 @@ export default {
         async updateStation(data) {
             await this.$store.dispatch({ type: 'updateStation', data })
             const { id } = this.$route.params
+            let msg;
+            console.log(data);
+
+            if (data.inNew) msg = `Added ${data.track.title} to ${data.station.name}`
+            if (!data.inNew) msg = `removed ${data.track.title} from ${data.station.name}`
+
+            eventBus.emit('show-msg', msg)
             this.station = await this.$store.getters.getStation(id)
 
         }
